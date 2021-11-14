f67aa86e6234f96c7f6be8e18a1ef6784bd6a531
SallyCodeP <phelipelipeoliveura@gmail.com>
2021-11-13 22:10:34 -0300

    I.A rs funcoes inicias sendo construidas

---
+++ I.A/main.py
@@ -0,0 +1,36 @@
+import numpy as np 
+
+def function(x):
+    return float(x*2)
+
+
+
+x = np.array([2.0, 3.0, 4.0, 5.0, 6.0], dtype=np.float32)
+y = np.array([4.0, 6.0, 8.0, 10.0, 12.0], dtype=np.float32)
+w = 0.0
+
+
+
+def foward(x):
+    return x*w
+
+print(foward(14))
+
+def loss(y, y_pred):
+    return ((y_pred - y)**2).mean()
+
+def grad(x, y, y_pred):
+    # usando regra da corrente para derivar w em loss
+    # e depois pegando a media dos valores
+    return np.dot(2*x, y_pred - y).mean()
+
+rate = 0.01
+repeticao = 8000
+
+for aprender in range(repeticao):
+    y_pred = foward(x)
+    l = loss(y, y_pred)
+    wd = grad(x, y, y_pred)
+    w -= wd * rate
+
+print(f"{foward(14):.3f}")
---
+++ I.A/rs/target/rls/debug/deps/rs-c3d2a738e141cf25.d
@@ -0,0 +1,5 @@
+/home/lemon/Area/all/creative_dev/Math_I.A/I.A/rs/target/rls/debug/deps/rs-c3d2a738e141cf25.rmeta: src/main.rs
+
+/home/lemon/Area/all/creative_dev/Math_I.A/I.A/rs/target/rls/debug/deps/rs-c3d2a738e141cf25.d: src/main.rs
+
+src/main.rs:
